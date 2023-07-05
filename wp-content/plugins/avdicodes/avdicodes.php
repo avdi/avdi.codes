88689ca199b4a9721f54fc614f8e3e60a7c42fbf
Avdi Grimm <avdi@avdi.codes>
2023-07-05 00:01:59 -0500

    Allow ebook mimetypes

---
+++ wp-content/plugins/avdicodes/avdicodes.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: avdi.codes site-specific plugin
+Description: A site-specific plugin for avdi.codes
+Version: 0.1
+Author: Avdi Grimm
+Author URI: https://avdi.codes
+*/
+
+function avdicodes_allow_ebook_file_uploads($mime_types){
+    $mime_types['epub'] = 'application/epub+zip'; 
+    $mime_types['mobi'] = 'application/x-mobipocket-ebook';
+    $mime_types['html'] = 'text/html';
+    return $mime_types;
+}
+add_filter('upload_mimes', 'avdicodes_allow_ebook_file_uploads', 1, 1);
