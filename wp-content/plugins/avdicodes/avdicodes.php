8e5b90f0cbf44e0a3b69d6ddaaf9b1f89412c4af
Avdi Grimm <avdi@avdi.codes>
2024-02-23 21:40:47 -0600

    add filter renaming MemberPress Subscriptions to Products

--- wp-content/plugins/avdicodes/avdicodes.php
+++ wp-content/plugins/avdicodes/avdicodes.php
@@ -7,10 +7,15 @@ Author: Avdi Grimm
 Author URI: https://avdi.codes
 */
 
+add_filter('upload_mimes', 'avdicodes_allow_ebook_file_uploads', 1, 1);
 function avdicodes_allow_ebook_file_uploads($mime_types){
     $mime_types['epub'] = 'application/epub+zip'; 
     $mime_types['mobi'] = 'application/x-mobipocket-ebook';
     $mime_types['html'] = 'text/html';
     return $mime_types;
 }
-add_filter('upload_mimes', 'avdicodes_allow_ebook_file_uploads', 1, 1);
+
+add_filter('mepr-account-nav-subscriptions-label', 'avdicodes_rename_subscriptions_to_products');
+function avdicodes_rename_subscriptions_to_products(): string {
+    return 'Products';
+}
