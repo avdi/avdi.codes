fd40395f89d992ef42a7b0b9f36a9222b55d373f
Avdi Grimm <avdi@avdi.codes>
2024-07-18 12:55:43 -0500

    Kludge for missing fpassthru() on Cloudways

--- cache-config.php
+++ cache-config.php
@@ -1,4 +1,9 @@
 <?php
+// A horrid kludge for the fact that Cloudways disables fpassthru()
+function fpassthru($stream) {
+	echo stream_get_contents($stream);
+}
+
 // Content negotiation support for Surge
 // via: https://dominikschilling.de/notes/http-accept-header-wordpress-cache-activitypub/
 $representation = 'html'; // Or 'generic'.
@@ -17,5 +22,7 @@ if ( isset( $_SERVER['HTTP_ACCEPT'] ) ) {
 }
 $config['variants']['representation'] = $representation;
 unset( $accept, $representation );
+
+$config['ignore_cookies'][] = 'wpf_ref';
 
 return $config;
